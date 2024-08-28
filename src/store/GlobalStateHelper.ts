2c6ebc93d80d63bc7c27b60ab723e4b7ce344328
Noel Schenk <schenknoel@gmail.com>
2024-08-28 15:29:03 +0200

    Update GlobalStateHelper.ts

--- src/store/GlobalStateHelper.ts
+++ src/store/GlobalStateHelper.ts
@@ -1,11 +1,20 @@
-type Primitive = string | number | boolean | symbol | undefined | null
+type Primitive = string | number | boolean | symbol | undefined | null;
 
-type Flatten<T, Path extends string = ''> = {
-    [K in keyof T]: T[K] extends Primitive | Array<any> | Map<any, any>
-        ? { [P in `${Path}${K & string}`]: T[K] }
-        : Flatten<T[K], `${Path}${K & string}.`>
-}[keyof T]
+type Flatten<T, Path extends string = ""> = {
+  [K in keyof T]: T[K] extends
+    | Primitive
+    | Array<any>
+    | Map<any, any>
+    | File
+    | Set<any>
+    ? { [P in `${Path}${K & string}`]: T[K] }
+    : Flatten<T[K], `${Path}${K & string}.`>;
+}[keyof T];
 
-type Merge<T> = (T extends any ? (x: T) => void : never) extends (x: infer R) => void ? R : never
+type Merge<T> = (T extends any ? (x: T) => void : never) extends (
+  x: infer R
+) => void
+  ? R
+  : never;
 
-export type FlattenType<T> = Merge<Flatten<T>>
+export type FlattenType<T> = Merge<Flatten<T>>;
